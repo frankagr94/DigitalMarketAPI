3a8f1beb72f889070174491993cc5748ff84b3a5
Your Name <you@example.com>
2018-06-12 00:38:01 -0400

    sequelize application6

--- models/index.js
+++ models/index.js
@@ -11,8 +11,7 @@ var Sequelize = require('sequelize');
 var sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect:  'postgres',
   protocol: 'postgres',
-  port:     match[4],
-  host:     match[3],
+  port:     5432,
   logging:  true, //false
 
   define: {
